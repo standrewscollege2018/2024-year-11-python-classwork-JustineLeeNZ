8527dffee4705ea883766b52412514dfd5d0c024
JustineLeeNZ <jlu@stac.school.nz>
2024-02-08 14:20:53 +1300

    Added print with a variable

--- basics1.py
+++ basics1.py
@@ -1,9 +1,16 @@
 # print out name of favourite book or movie
-print("Star Wars")
+print("My favourite movie is Star Wars")
 
 
 # ask the user for their favourite book movie etc
 movie = input("What is your favourite movie?")
 
 # display a friendly message
-print("That' cool. I like that movie too")
+print("That' cool. I like the", movie, "movie too")
+
+# ask the user for their age
+age = input("How old are you?")
+age = int(age)
+
+# display message telling their age in 2050
+print("In 2050 you will be", age+26, "years old")
