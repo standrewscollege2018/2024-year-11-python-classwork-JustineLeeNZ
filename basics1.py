22be6b8521f4a1a52ce6993182d71a07ef3df49c
JustineLeeNZ <jlu@stac.school.nz>
2024-02-08 14:03:59 +1300

    added input to code
    asked user for favourite movie

--- basics1.py
+++ basics1.py
@@ -2,4 +2,8 @@
 print("Star Wars")
 
 
-#
+# ask the user for their favourite book movie etc
+movie = input("What is your favourite movie?")
+
+# display a friendly message
+print("That' cool. I like that movie too")
