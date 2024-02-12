e1d3535af57bc9d984b7aed9e7b866175fd2e279
JustineLeeNZ <jlu@stac.school.nz>
2024-02-12 18:11:17 +1300

    Used f-strings in print statements

--- basics1.py
+++ basics1.py
@@ -2,15 +2,24 @@
 print("My favourite movie is Star Wars")
 
 
+# ask user for name
+name = input("Name?")
+
 # ask the user for their favourite book movie etc
 movie = input("What is your favourite movie?")
 
 # display a friendly message
-print("That' cool. I like the", movie, "movie too")
+#print("That's cool", name, ". I like the", movie, "movie too")
+# same message but using a f-string - MUCH NICER TO USE!!!
+print(f"That's cool {name}. I like the {movie} movie too")
 
 # ask the user for their age
 age = input("How old are you?")
 age = int(age)
+print(age)
 
 # display message telling their age in 2050
-print("In 2050 you will be", age+26, "years old")
+#print("In 2050 you will be", age+26, "years old")
+
+# use f-string to add their name and also to display the age+26
+print(f"{name}, in 2050 you will be {age+26} years old")
