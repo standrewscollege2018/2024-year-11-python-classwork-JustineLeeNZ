b30a08edd2779922b66f3c78d40bf05232b112af
JustineLeeNZ <jlu@stac.school.nz>
2024-02-18 22:24:28 +1300

    removed some unnecessary print statements

--- basics1.py
+++ basics1.py
@@ -16,10 +16,6 @@ print(f"That's cool {name}. I like the {movie} movie too")
 # ask the user for their age
 age = input("How old are you?")
 age = int(age)
-print(age)
-
-# display message telling their age in 2050
-#print("In 2050 you will be", age+26, "years old")
 
 # use f-string to add their name and also to display the age+26
 print(f"{name}, in 2050 you will be {age+26} years old")
