90abb4b31ed217634df8d5b9883e6b9b3b6f09e6
JustineLeeNZ <jlu@stac.school.nz>
2024-02-18 22:23:16 +1300

    simple version of age in 2050

---
+++ age_in_2050.py
@@ -0,0 +1,11 @@
+""" displays how old user will be in 2050 - Justine Lee """
+
+# ask user for name
+name = input("Name?")
+
+# ask the user for their age
+age = input("How old are you?")
+age = int(age)
+
+# display their name and also their age in 2050
+print(f"{name}, in 2050 you will be {age+26} years old")
