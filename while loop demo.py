876e131f1935dcc08280fb104978f37645732795
JustineLeeNZ <jlu@stac.school.nz>
2024-02-21 09:19:33 +1300

    While loop demo code

---
+++ while loop demo.py
@@ -0,0 +1,33 @@
+""" demo of while loops - Justine Lee """
+
+# not a useful way to use while loop
+# for loop does this better
+"""
+i = 0
+
+while i < 10:
+    print(i)
+    i = i + 1
+
+print("All done")
+"""
+
+
+# ask user for name and print hello message
+
+keep_asking = True
+
+while keep_asking == True:
+    name = input("Enter your name: ")
+
+
+    if name == "stop":
+        keep_asking = False
+    else:
+        print(f"Hello {name}")
+
+
+
+
+
+
