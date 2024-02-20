bb58fbc3d76e7326a14d4939d076655c5fe94428
JustineLeeNZ <jlu@stac.school.nz>
2024-02-21 08:49:35 +1300

    created code demo - for loops

---
+++ for loop demo.py
@@ -0,0 +1,25 @@
+""" demo of for loops - Justine Lee """
+
+
+# prints 0-9
+print("last value is end value -1 i.e. 9")
+for i in range(0,10):
+    print(i)
+
+# prints 1-10
+print("starts at 1, end value is 10+1")
+for i in range(1,11):
+    print(i)
+
+# skip counts in 2s from 2-20
+print("skip counts")
+for i in range(1,11):
+    print(i*2)
+
+# skip counts in 2 from 1-9
+for i in range(1,11,2):
+    print(i)
+
+
+
+
